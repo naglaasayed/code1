efd7044845876096c8564c752f8ab1fba9fd1083
naglaasayed <36450220+naglaasayed@users.noreply.github.com>
2018-03-29 16:45:41 +0200

    Add files via upload

---
+++ book proplem g12.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<string>
+#include<cmath>
+using namespace std;
+int main(){
+double value of pi,n;
+char repeat;
+cout<<"enter the numof items  ";
+cin>>n;
+for(int i=o;i<n;i++)
+    {
+        value of pi +=(4.0(pow(-1.0i)/(2.0*i+1)));
+
+    }
+cout<<"value of pi for first "<<n<<"terms"<<value of pi<<endl;
+cout<<"do you want to repeat(y,n)";
+cin>>repeat;
+}while(repeat=="y"||"n")
+cout<<"thank you"<<endl;
+return 0
+}
+
